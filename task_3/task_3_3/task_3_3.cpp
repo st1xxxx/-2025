78ccf24e2cb7bd496d3680c73e7806db78fa5a99
Скворцов Серафим Денисович <serafimskvortsov123@gmail.com>
2025-11-24 22:58:30 +0300

    update

--- task_3/task_3_3/task_3_3.cpp
+++ task_3/task_3_3/task_3_3.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
 #include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 #include <limits>
 #include <windows.h>
 
@@ -15,30 +18,68 @@ void printStudentInfo() {
 }
 
 
-template<typename T>
-void printVector(const vector<T>& vec) {
-    cout << "My vector has " << vec.size() << " of these elements:" << endl;
-    for (size_t i = 0; i < vec.size(); i++) {
-        cout << "[" << i << "] -> " << vec[i] << endl;
-    }
-}
+
 
 int main() {
     printStudentInfo();
+    srand(time(nullptr));
     
     
-    vector<int> intVec = {1, 2, 3, 4, 5};
-    vector<string> strVec = {"apple", "banana", "cherry"};
-    vector<double> doubleVec = {1.1, 2.2, 3.3, 4.4};
+    ifstream inputFile("books.txt");
+    if (!inputFile.is_open()) {
+        cout << "Ошибка: файл 'books.txt' не найден!" << endl;
+        cout << "Сначала выполните задание 1." << endl;
+        cout << "\nНажмите Enter для выхода...";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin.get();
+        return 1;
+    }
     
-    cout << "Вектор int:" << endl;
-    printVector(intVec);
+    vector<string> names;
+    vector<string> authors;
+    vector<string> years;
     
-    cout << "\nВектор string:" << endl;
-    printVector(strVec);
+    string line;
+    while (getline(inputFile, line)) {
+        if (line.find("Name:") != string::npos) {
+            names.push_back(line.substr(line.find("Name:") + 6));
+        } else if (line.find("Author:") != string::npos) {
+            authors.push_back(line.substr(line.find("Author:") + 8));
+        } else if (line.find("Year:") != string::npos) {
+            years.push_back(line.substr(line.find("Year:") + 6));
+        }
+    }
+    inputFile.close();
     
-    cout << "\nВектор double:" << endl;
-    printVector(doubleVec);
+    
+    for (int i = names.size() - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        swap(names[i], names[j]);
+    }
+    
+    for (int i = authors.size() - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        swap(authors[i], authors[j]);
+    }
+    
+    for (int i = years.size() - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        swap(years[i], years[j]);
+    }
+    
+    
+    ofstream outputFile("incorrect_books.txt");
+    
+    for (size_t i = 0; i < names.size(); i++) {
+        outputFile << (i + 1) << ". Name:" << names[i] << endl;
+        outputFile << "   Author:" << authors[i] << endl;
+        outputFile << "   Year:" << years[i] << endl;
+        outputFile << "   ----------" << endl;
+    }
+    
+    outputFile.close();
+    
+    cout << "Файл 'incorrect_books.txt' успешно создан с перемешанными данными." << endl;
     
     cout << "\nНажмите Enter для выхода...";
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
