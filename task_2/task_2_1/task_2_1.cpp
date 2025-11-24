558c7879e5b6ce293cd68ef881783b104df40013
Скворцов Серафим Денисович <serafimskvortsov123@gmail.com>
2025-11-24 21:48:59 +0300

    update

--- task_2/task_2_1/task_2_1.cpp
+++ task_2/task_2_1/task_2_1.cpp
@@ -1,46 +1,59 @@
 #include <iostream>
+#include <map>
 #include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 #include <limits>
-#ifdef _WIN32
 #include <windows.h>
-#endif
 
 using namespace std;
 
 void printStudentInfo() {
+    SetConsoleOutputCP(CP_UTF8);
     cout << "Студент: Скворцов Серафим Денисович" << endl;
     cout << "Группа: M10-137БВ-25" << endl;
     cout << "Задача: 1" << endl;
     cout << endl;
 }
 
-void printMatrix(const vector<vector<int>>& matrix) {
-    for (const auto& row : matrix) {
-        for (const auto& element : row) {
-            cout << element << '\t';
+int main() {
+    printStudentInfo();
+    srand(time(nullptr));
+    
+    map<string, vector<int>> students;
+    int studentCount = 3; 
+    int gradesCount = 5;
+    
+    cout << "Введите фамилии " << studentCount << " студентов:" << endl;
+    
+    for (int i = 0; i < studentCount; i++) {
+        string surname;
+        cout << "Студент " << (i + 1) << ": ";
+        cin >> surname;
+        
+        vector<int> grades;
+        for (int j = 0; j < gradesCount; j++) {
+            grades.push_back(rand() % 4 + 2); 
+        }
+        
+        students[surname] = grades;
+    }
+    
+    cout << "\nСписок группы и успеваемость:" << endl;
+    cout << "=============================" << endl;
+    
+    for (const auto& student : students) {
+        cout << student.first << ": ";
+        for (int grade : student.second) {
+            cout << grade << " ";
         }
         cout << endl;
     }
-}
-
-int main() {
-#ifdef _WIN32
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
-#endif
-    printStudentInfo();
-
-    vector<vector<int>> m = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-
-    cout << "Matrix contents:" << endl;
-    printMatrix(m);
-    cout << "\nPress Enter to exit...";
+    
+    cout << "\nНажмите Enter для выхода...";
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cin.get();
-
+    
     return 0;
 }
